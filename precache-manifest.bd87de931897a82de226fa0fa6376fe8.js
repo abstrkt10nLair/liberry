c5d646de5ac4846dce776cdc1ec47992747ea207
Bilal Belagam <bilal.belagam@gmail.com>
2020-10-18 17:45:12 -0400

    Updates

---
+++ precache-manifest.bd87de931897a82de226fa0fa6376fe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15885c1dd62ca95c7e80c79f9955f031",
+    "url": "/liberry/index.html"
+  },
+  {
+    "revision": "69167cb80ad6e3390802",
+    "url": "/liberry/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2f9c504527c464fdd1c4",
+    "url": "/liberry/static/js/2.6cffebcd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/liberry/static/js/2.6cffebcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69167cb80ad6e3390802",
+    "url": "/liberry/static/js/main.ef6047d8.chunk.js"
+  },
+  {
+    "revision": "967a85fdd4e8bc62f139",
+    "url": "/liberry/static/js/runtime-main.576ec784.js"
+  }
+]);
